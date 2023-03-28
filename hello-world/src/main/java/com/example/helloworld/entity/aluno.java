188cb616f96eb32e8958af2d086567fb05d6ed6a
Felipe da Hora <lipedahora2000@hotmail.com>
2023-03-28 20:40:16 -0300

    criacao entidade aluno

---
+++ hello-world/src/main/java/com/example/helloworld/entity/aluno.java
@@ -0,0 +1,26 @@
+package com.example.helloworld.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class aluno {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataNascimento;
+
+    
+    
+}
