fe2233623c42ed671c6cdf0a1fe14268a8b95986
Felipe da Hora <lipedahora2000@hotmail.com>
2023-03-28 21:31:03 -0300

    criacao service

---
+++ hello-world/src/main/java/com/example/helloworld/service/AlunoService.java
@@ -0,0 +1,35 @@
+package com.example.helloworld.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.stereotype.Service;
+
+import com.example.helloworld.entity.Aluno;
+import com.example.helloworld.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+    
+    public List<Aluno> listarTodos(){
+        return alunoRepository.findAll();
+    }
+
+    public void excluir(Long id){
+        Aluno aluno = alunoRepository.findById(id).get();
+        alunoRepository.delete(aluno);
+    }
+
+    public Aluno salvar(Aluno aluno){
+        return alunoRepository.saveAndFlush(aluno);
+    }
+
+    public Aluno atualizar(Aluno aluno) {
+        return alunoRepository.saveAndFlush(aluno);
+    }
+    
+}
